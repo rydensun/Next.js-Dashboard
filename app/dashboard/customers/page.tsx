245dac5135f7bbd96d5131bcc50626bec6bec8b1
rydensun <rydensun@gmail.com>
2024-06-17 21:57:05 +0800

    add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
+export default async function Page() {
+  const customerData = await fetchCustomers();
+  console.log('customerdata', customerData);
 }
